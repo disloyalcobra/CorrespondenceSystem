db9d376dbe9913a2daa0b1ea9c9bab8645899c10
disloyalcobra <josepablomateosgamboa@gmail.com>
2026-09-22 10:50:19 -0600

    chore: add relative base path to vite config and vscode launch configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Añadido para que las rutas de los assets sean relativas en GitHub Pages
   server: {
     port: 3000,
     open: false
